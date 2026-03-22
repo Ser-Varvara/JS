9caf5a8224ce808dfa3b0a81ad991ff656088dad
Ser-Varvara <145899687+Ser-Varvara@users.noreply.github.com>
2026-03-22 12:23:43 +0200

    Update script.js

--- lab3/script.js
+++ lab3/script.js
@@ -1,3 +1,27 @@
 (function () {
-  console.log("Скрипт завантажено!");
+  var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+
+  console.log("--- Основне завдання: перевірка на літеру 'J' ---");
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+
+  console.log("\n--- Додаткове завдання: перевірка за останньою літерою ---");
+  console.log("Анотація: Якщо ім'я закінчується на 'a' або 'n', виводимо особливе повідомлення.");
+  
+  for (var i = 0; i < names.length; i++) {
+    var lastLetter = names[i].charAt(names[i].length - 1).toLowerCase();
+    
+    if (lastLetter === 'a' || lastLetter === 'n') {
+      console.log("Special welcome for " + names[i]);
+    } else {
+      console.log("Regular hello for " + names[i]);
+    }
+  }
 })();
